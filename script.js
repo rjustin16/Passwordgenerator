485013e9b0ea3debe100bcbe34f5ee435ab03863
Ricky Justiniano <justiniano.ricky@gmail.com>
2020-11-06 12:42:10 -0700

    fix

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ form.addEventListener("submit", e => {
     // run generate password function
     const password = generatePassword(passwordlength, Uppercase, Numbers, Symbols)
     // display password variable from generate password function in password display
-    passwordText.innerText = password
+    passwordDisplay.innerText = password
 
 })
 // variables for the characters for the password

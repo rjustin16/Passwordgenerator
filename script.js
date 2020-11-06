3fada20fb240568ca293486fe930dd3e927cc5d6
Ricky Justiniano <justiniano.ricky@gmail.com>
2020-11-05 20:18:39 -0700

    read me update and instructions

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
-
-const characterNumber = document.getElementById("characterAmountNumber")
+// accessing elements in html
+const characterNumber = document.getElementById("characterNumber")
 const UppercaseElement = document.getElementById("Uppercase")
 const NumbersElement = document.getElementById("Numbers")
 const SymbolsElement = document.getElementById("Symbols")
 const form = document.getElementById("passwordGeneratorForm")
-const passwordDipslay = document.getElementById("passwordDisplay")
+const passwordDipslay = document.getElementById("passwordText")
 
-characterNumber.addEventListener("input", sycnCharacter)
 
-function sycnCharacter(e) {
-    const value = e.target.value
-    characterNumber.value = value
-}
-
+// Generate Password Button
 form.addEventListener("submit", e => {
+    // stop reload
     e.preventDefault()
+    // add character amount by the value added
     const characterAmount = characterNumber.value
+    // if uppercase checked add uppercase
     const Uppercase = UppercaseElement.checked
+     // if number checked add number
     const Numbers = NumbersElement.checked
+    // if symbol checked add symbol
     const Symbols = SymbolsElement.checked
+    // run generate password function
     const password = generatePassword(characterAmount, Uppercase, Numbers, Symbols)
-    passwordDipslay.innerText = password
+    // display password variable from generate password function in password display
+    passwordText.innerText = password
 
 })
 
+// array to get every character between charCode list
+function arrayFromLowToHigh(low, high){
+        const array = []
+        for (let i = low; i <= high; i++){
+            array.push(i)
+        }
+        return array
+    }
+
+
+// charCode list for each parameter
 const lower = arrayFromLowToHigh(97, 122)
 const upper = arrayFromLowToHigh(65, 90)
 const number = arrayFromLowToHigh(48, 57)
@@ -34,25 +47,27 @@ const symbol = arrayFromLowToHigh(33, 47).concat(
     ).concat(
     arrayFromLowToHigh(123, 126)
     )
-        
-    function arrayFromLowToHigh(low, high){
-        const array = []
-        for (let i = low; i <= high; i++){
-            array.push(i)
-        }
-        return array
-    }
+
+
     
-
+// random generator function
 function generatePassword(characterAmount, Uppercase, Numbers, Symbols) {
-    let charCodes = lower
-    if (Uppercase) charCodes = charCodes.concat(upper)
-    if (Numbers) charCodes = charCodes.concat(number)
-    if (Symbols) charCodes = charCodes.concat(symbol)
+    // default to lowercase
+    let ref = lower
+    // if uppercase is true add uppercase from char 
+    if (Uppercase) ref = ref.concat(upper)
+    // if number is true add number from char codes
+    if (Numbers) ref = ref.concat(number)
+    // if symbol is true add symbol from char codes
+    if (Symbols) ref = ref.concat(symbol)
 
+    // empty array to pass password characters through
     const passwordCharacters = []
+    // number of characters created equal the character amount
     for (let i = 0; i < characterAmount; i++) {
-      const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+        // randomize the characters according to ref rule
+      const characterCode = ref[Math.floor(Math.random() * ref.length)]
+    //   have characterCode pull from the char code reference
       passwordCharacters.push(String.fromCharCode(characterCode))
     }
     return passwordCharacters.join("")
